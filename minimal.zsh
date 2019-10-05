b9303637d954fb38e468e63eed05b7b5b76edd34
Giuseppe Sorrentino <uhpnou@gmail.com>
2019-10-05 12:29:27 +0200

    improved git prompt

--- minimal.zsh
+++ minimal.zsh
@@ -128,9 +128,9 @@ git_prompt(){
   if [[ $(plib_is_git) == 1 ]]; then
     git_prompt_val+="%F{$MINIMAL_FADE_COLOR}[%f "
     git_prompt_val+="%f$(plib_git_branch)"
-    [[ $(plib_git_stash) == 1 ]] && git_prompt_val+=" ${MINIMAL_GIT_STASH_SYM} "
-    [[ ! -z $(plib_git_left_right) ]] && git_prompt_val+=" %F{red}$(minimal_git_left_right)%f "
-    git_prompt_val+="%F{$MINIMAL_FADE_COLOR}]%f"
+    [[ $(plib_git_stash) == 1 ]] && echo cacca && git_prompt_val+=" ${MINIMAL_GIT_STASH_SYM}"
+    [[ ! -z $(minimal_git_left_right) ]] && echo cacca2 && git_prompt_val+=" %F{red}$(minimal_git_left_right)%f"
+    git_prompt_val+=" %F{$MINIMAL_FADE_COLOR}]%f"
   fi
   PROMPT="${PROMPT}${git_prompt_val}"
 }
